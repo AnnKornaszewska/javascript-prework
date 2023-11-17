c5827ef9db7336ccbb5a84e0713e120e52811bfe
Anna Kornaszewska <annkornaszewska@gmail.com>
2023-11-17 12:42:38 +0100

    Add commit 2

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, PlayerMove, set_playerMove_to;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+PlayerMove = 'papier';
+printMessage('Zagrałam' + PlayerMove + '! Wygrałam!');
