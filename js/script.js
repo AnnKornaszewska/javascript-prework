b6aaa5691b7382202c25fd0828d77246d4b42a41
Anna Kornaszewska <annkornaszewska@gmail.com>
2023-11-27 22:56:48 +0100

    displayResult function optimized

--- js/script.js
+++ js/script.js
@@ -9,25 +9,6 @@ let argButtonName;
 
 function buttonClicked(argButtonName) {
 
-	let argComputerMove, argPlayerMove, computerMove, playerInput, playerMove;
-
-	// ta funkcja zwraca wynik w oparciu o ruch gracza oraz ruch komputera
-	function displayResult(argPlayerMove, argComputerMove) {
-		console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-		if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-			printMessage('Wygrywasz!');
-		} else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-			printMessage('Wygrywasz!');
-		} else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-			printMessage('Wygrywasz!');
-		} else if (argPlayerMove == argComputerMove && argComputerMove == argPlayerMove) {
-			printMessage('Remis!');
-		} else {
-			printMessage('Przegrywasz :(');
-		}
-		printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-		}
-
 	// ta funkcja zwraca ruch losowy komputera
 	function getMoveName() {
 
@@ -46,16 +27,34 @@ function buttonClicked(argButtonName) {
 		}
 		}
 
+	// ta funkcja zwraca wynik w oparciu o ruch gracza oraz ruch komputera
+	function displayResult() {
+
+		console.log('wywołano funkcję displayResults');
+
+		let playerMove = argButtonName;
+		console.log('ruch gracza to: ' + playerMove);
+
+		let computerMove = getMoveName();
+		console.log('ruch komputera to: ' + computerMove);
+
+		if (playerMove == 'papier' && computerMove == 'kamień') {
+			printMessage('Wygrywasz!');
+		} else if (playerMove == 'kamień' && computerMove == 'nożyce') {
+			printMessage('Wygrywasz!');
+		} else if (playerMove == 'nożyce' && computerMove == 'papier') {
+			printMessage('Wygrywasz!');
+		} else if (playerMove == computerMove && computerMove == playerMove) {
+			printMessage('Remis!');
+		} else {
+			printMessage('Przegrywasz :(');
+		}
+		printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove);
+		}
 
 	clearMessages();
 	console.log(argButtonName + ' został kliknięty');
-
-	playerMove = argButtonName;
-	console.log('ruch gracza to: ' + playerMove);
-	computerMove = getMoveName();
-	console.log('ruch komputera to: ' + computerMove);
-	displayResult(playerMove, computerMove);
-
+	displayResult();
 
 	}
 
